731159c15db942dfdb7a2c13b796dc28e3825ad1
ardaatay <atay_arda@outlook.com>
2021-06-14 17:20:37 +0300

    14 Haziran

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { Container, Grid } from 'semantic-ui-react'
-import ProductList from '../pages/ProductList'
-import Categories from './Categories'
+import React from "react";
+import { Container, Grid } from "semantic-ui-react";
+import ProductList from "../pages/ProductList";
+import Categories from "./Categories";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Container className="main">
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={4}>
-                            <Categories />
-                        </Grid.Column>
-                        <Grid.Column width={12}>
-                            <ProductList />
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <Container className="main">
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <Categories />
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <ProductList />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
